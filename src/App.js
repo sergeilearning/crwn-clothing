9d4e1940b8c5c0c5ce98c02ea2ac14b4fed9d4a9
Sergei G <srgthe@gmail.com>
2019-10-30 16:48:00 -0700

    Add Router to the menu from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage.component";
 
+const HatsPage = () => {
+  return <h1>Hats Page</h1>;
+};
+
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact component={HomePage} path="/" />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
