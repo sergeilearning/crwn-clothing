bc717162ca9cb35b8e2dafd4bd382bc91be9bfc8
Sergei G <srgthe@gmail.com>
2019-10-30 14:05:45 -0700

    Create components, split folders and add styling to menu

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from "../components/directory/directory.component";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <h1>Welcome to CRWN clothing</h1>
+      <Directory />
+    </div>
+  );
+};
+export default HomePage;
