04fac6be5094e0c4da1f1aabdfe33ba5b91380c1
Sergei G <srgthe@gmail.com>
2019-11-25 15:18:49 -0800

    Implement Redux thunk into the project and add containers for collections components

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
+//import createSagaMiddleware from "redux-saga";
+import thunk from "redux-thunk";
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [];
+//const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [thunk];
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
